86d6f23aa608eb94a1d3a446b2c2987458e1d776
NuvensCorporation <nuvenscorporation@gmail.com>
2022-05-02 11:36:17 -0300

    first commit

--- app/Models/Transfer.php
+++ app/Models/Transfer.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transfer extends Model
 {
-    protected $table = "transfer";
+    protected $table = "transaction_history";
     
     protected $fillable = [
         'amount', 'order_number','ip_address','sender_id','receiver_id','currency_id','created_at','updated_at'
